e887f5e94cfd76facb6e5ba66f0ce441b07c8e2e
Papp Imre <imi950127@gmail.com>
2019-10-10 14:48:13 +0200

    Finished add log route

--- routes/logs.js
+++ routes/logs.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser').json();
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
-const Log = require('../models/logsModel');
+const Log = require('../models/Log');
 
 // @route       GET api/logs
 // @desc        Get all logs
@@ -20,9 +21,41 @@ router.get('/', async (req, res) => {
 // @route       POST api/logs
 // @desc        Add a log
 // @access      Public
-router.post('/', (req, res) => {
-  res.send('Add a log');
-});
+router.post(
+  '/',
+  [
+    bodyParser,
+    [
+      check('message', 'Message is required')
+        .not()
+        .isEmpty(),
+      check('tech', 'A technician is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { message, attention, tech } = req.body;
+
+    try {
+      const newLog = new Log({
+        message,
+        attention,
+        tech
+      });
+      const log = await newLog.save();
+      res.json(log);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 // @route       PUT api/logs/:id
 // @desc        Update a log
